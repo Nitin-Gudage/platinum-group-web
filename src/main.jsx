5aae9ccbf400569548bb5c56eac25e8b934a19d4
unknown <gudagenitin11@gmail.com>
2026-02-09 16:28:12 +0530

    Update UI and fixes 9 feb final deployable

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
+
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
-import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <Router>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </Router>
-    </HelmetProvider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </StrictMode>,
 );
