9138be7ab44b574d31044f6112b845cfc0c83f49
unknown <gudagenitin11@gmail.com>
2026-02-11 01:48:08 +0530

    skip to containt button remove

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,44 @@ import "./App.css";
 
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import Home from "./pages/Home";
 import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
-import { Route, Routes, useLocation } from "react-router-dom";
 import FaqPage from "./pages/FaqPage";
-import { useEffect, useCallback } from "react";
 import ServicesPage from "./pages/ServicesPage";
+
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 import SeoSchema from "./utils/SeoSchema";
 import StickyCallButton from "./components/StickyCallButton";
 
 function App() {
   const { pathname, search } = useLocation();
 
-  /* Instant scroll to top on route change - optimized for performance */
+  /* Scroll to top on route change */
   useEffect(() => {
-    // Use instant scroll instead of smooth for faster navigation
     window.scrollTo(0, 0);
   }, [pathname, search]);
 
   return (
     <div className="App bg-gray-50 min-h-screen flex flex-col">
-      {/* Skip to main content link for accessibility */}
-      <a
-        href="#main-content"
-        className="sr-only focus:not-sr-only focus:absolute focus:top-0 focus:left-0 focus:z-[9999] focus:px-4 focus:py-2 focus:bg-blue-600 focus:text-white focus:outline-none focus:ring-2 focus:ring-yellow-400"
-      >
-        Skip to main content
-      </a>
+
       <NavBar />
       <SeoSchema />
 
-      {/* Optimized Background - removed heavy blur effects */}
+      {/* Background Effects */}
       <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
         <div className="absolute -top-40 -right-40 w-96 h-96 bg-blue-500/5" />
         <div className="absolute top-1/2 -left-40 w-96 h-96 bg-cyan-500/5" />
         <div className="absolute -bottom-40 right-1/3 w-96 h-96 bg-blue-400/5" />
       </div>
 
-      <main id="main-content" className="flex-grow">
+      {/* Main Content */}
+      <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<ServicesPage />} />
@@ -54,18 +51,20 @@ function App() {
 
       <Footer />
       <StickyCallButton />
+
+      {/* Toast Notifications */}
       <ToastContainer
         position="top-right"
         autoClose={3000}
         hideProgressBar={false}
-        newestOnTop={true}
+        newestOnTop
         closeOnClick
-        rtl={false}
         pauseOnFocusLoss
         draggable
         pauseOnHover
         theme="colored"
       />
+
     </div>
   );
 }
